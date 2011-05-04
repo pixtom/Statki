ff4211881f4ff3eeef4234ea7d53b32e731f9c1b
Wójtowicz Tomasz <tomash2310w@gmail.com>
2011-05-04 21:11:40 +0200

    Kawalek menu - proba. Ide na mecz :)

--- src/statki/StatkiView.java
+++ src/statki/StatkiView.java
@@ -5,7 +5,11 @@
 
 package statki;
 
+import java.awt.Container;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  *
@@ -16,9 +20,33 @@ public class StatkiView extends JFrame {
     {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-        
+
+        initMenu();
 
         setSize(800,600);
+    }
+
+    private void initMenu()
+    {
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu fileMenu = new JMenu("Plik");
+        JMenuItem newGameItem = new JMenuItem("Nowa gra");
+        fileMenu.add(newGameItem);
+        JMenuItem closeItem = new JMenuItem("Zamknij");
+        fileMenu.add(closeItem);
+        menuBar.add(fileMenu);
+        
+        JMenu gameTypeMenu = new JMenu("Typ Gry");
+        menuBar.add(gameTypeMenu);
+        
+        JMenu applicationTypeMenu = new JMenu("Typ Aplikacji");
+        menuBar.add(applicationTypeMenu);
+
+        JMenu helpMenu = new JMenu("Pomoc");
+        menuBar.add(helpMenu);
+
     }
 
 }
