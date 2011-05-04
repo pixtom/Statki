f7c3e2d62ed8963bf3979408ad26bcd40ca7698d
Mariusz Tasak <pixelmt@gmail.com>
2011-05-04 11:48:33 +0200

    First commit

---
+++ src/statki/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package statki;
+
+/**
+ *
+ * @author pixel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+
+}
