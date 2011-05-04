44f7db6219b2fa3560b9548c50ec8d8f9f76b1ae
Wójtowicz Tomasz <tomash2310w@gmail.com>
2011-05-04 12:58:57 +0200

    konflikt test 2

--- src/statki/Main.java
+++ src/statki/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hello statki!");
+        System.out.print("Hello");
     }
 
 }
