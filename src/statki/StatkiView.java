bc64ec0256a55eeb4322642ae6b1f3f4dcc83600
Wójtowicz Tomasz <tomash2310w@gmail.com>
2011-05-05 19:40:27 +0200

    Kolejne itemy w menu

--- src/statki/StatkiView.java
+++ src/statki/StatkiView.java
@@ -6,10 +6,14 @@
 package statki;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
 
 /**
  *
@@ -32,20 +36,65 @@ public class StatkiView extends JFrame {
         setJMenuBar(menuBar);
 
         JMenu fileMenu = new JMenu("Plik");
+        menuBar.add(fileMenu);
         JMenuItem newGameItem = new JMenuItem("Nowa gra");
         fileMenu.add(newGameItem);
+        newGameItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+        });
         JMenuItem closeItem = new JMenuItem("Zamknij");
         fileMenu.add(closeItem);
-        menuBar.add(fileMenu);
-        
+        closeItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+
+        JMenu settingsMenu = new JMenu("Ustawienia");
+        menuBar.add(settingsMenu);
         JMenu gameTypeMenu = new JMenu("Typ Gry");
-        menuBar.add(gameTypeMenu);
-        
+        settingsMenu.add(gameTypeMenu);
+
+        ButtonGroup gameTypeGroup = new ButtonGroup();
+        JRadioButtonMenuItem HvsC_Item = new JRadioButtonMenuItem("Human vs Computer");
+        HvsC_Item.setSelected(true);
+        gameTypeGroup.add(HvsC_Item);
+        gameTypeMenu.add(HvsC_Item);
+
+        JRadioButtonMenuItem HvsH_Item = new JRadioButtonMenuItem("Human vs Human");
+        gameTypeGroup.add(HvsH_Item);
+        gameTypeMenu.add(HvsH_Item);
+
+        settingsMenu.addSeparator();
+
+        ButtonGroup applicationType = new ButtonGroup();
         JMenu applicationTypeMenu = new JMenu("Typ Aplikacji");
-        menuBar.add(applicationTypeMenu);
+        settingsMenu.add(applicationTypeMenu);
+
+        JRadioButtonMenuItem clientItem = new JRadioButtonMenuItem("Klient");
+        clientItem.setSelected(true);
+        applicationType.add(clientItem);
+        applicationTypeMenu.add(clientItem);
+
+        JRadioButtonMenuItem serverItem = new JRadioButtonMenuItem("Serwer");
+        applicationType.add(serverItem);
+        applicationTypeMenu.add(serverItem);
 
         JMenu helpMenu = new JMenu("Pomoc");
         menuBar.add(helpMenu);
+        JMenuItem aboutItem = new JMenuItem("About");
+        helpMenu.add(aboutItem);
+        aboutItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+        });
+        
 
     }
 
