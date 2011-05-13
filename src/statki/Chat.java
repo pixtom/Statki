534cb71141fb65c0f6b8f3c96f53a90bfbda0caa
daro788 <daro788@gmail.com>
2011-05-13 11:28:17 +0200

    pierwsza proba

--- src/statki/Chat.java
+++ src/statki/Chat.java
@@ -1,14 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package statki;
 
 /**
- *
- * @author pixel
+ * @author daro ;)
  */
+
+void cos();
+
 public class Chat {
 
 }
